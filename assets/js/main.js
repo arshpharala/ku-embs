dce6255d7cb7c97614515a73caf6eb30b2e518b0
Arshdeep Singh <arshdeepjhagra@gmail.com>
2025-09-13 13:45:39 +0400

    files

---
+++ assets/js/main.js
@@ -0,0 +1,6 @@
+// ================= HERO MENU TOGGLE =================
+$(document).ready(function () {
+  $(".hero__menu-toggle").on("click", function () {
+    $(this).toggleClass("active");
+  });
+});
